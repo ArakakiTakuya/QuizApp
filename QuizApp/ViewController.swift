ca688047dfb57e40a6e80dc386e843db8a5ad88f
ArakakiTakuya <225arataku@gmail.com>
2020-09-11 19:06:03 +0900

    added option button and stayed it

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var option1Button: UIButton!
+    @IBOutlet weak var option2Button: UIButton!
+    @IBOutlet weak var option3Button: UIButton!
+    @IBOutlet weak var option4Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        option1Button.layer.cornerRadius = 10.0
+        option1Button.layer.borderColor = UIColor.lightGray.cgColor
+        option1Button.layer.borderWidth = 2.0
+        option2Button.layer.cornerRadius = 10.0
+        option2Button.layer.borderColor = UIColor.lightGray.cgColor
+        option2Button.layer.borderWidth = 2.0
+        option3Button.layer.cornerRadius = 10.0
+        option3Button.layer.borderColor = UIColor.lightGray.cgColor
+        option3Button.layer.borderWidth = 2.0
+        option4Button.layer.cornerRadius = 10.0
+        option4Button.layer.borderColor = UIColor.lightGray.cgColor
+        option4Button.layer.borderWidth = 2.0
     }
 
 
